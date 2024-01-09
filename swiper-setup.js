f6ce1f718ef14f364d0e060e5b008be92de9d688
fs-sergey <125083142+fs-sergey@users.noreply.github.com>
2024-01-09 20:54:41 +0100

    Slider fixed

--- swiper-setup.js
+++ swiper-setup.js
@@ -1,24 +1,12 @@
 $(document).ready(function () {
   var mySwiper = new Swiper(".is-slider-main", {
-    // Optional parameters
-    direction: "horizontal", // 'vertical' for vertical slider
-    loop: true, // Enable continuous loop mode
-
-    // If you want to add pagination
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true, // So you can click on the pagination bullets
-    },
-
-    // Navigation arrows
+    direction: "horizontal",
+    loop: false,
+    slidesPerView: "auto",
+    spaceBetween: 16,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-    // And if you need a scrollbar
-    scrollbar: {
-      el: ".swiper-scrollbar",
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev",
     },
   });
 });
