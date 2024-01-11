03ca76cfec065622cbb47ab86e7e10aebea5ff00
fs-sergey <125083142+fs-sergey@users.noreply.github.com>
2024-01-11 11:58:44 +0100

    Update swiper-setup.js

--- swiper-setup.js
+++ swiper-setup.js
@@ -9,4 +9,21 @@ $(document).ready(function () {
       prevEl: ".swiper-prev",
     },
   });
+    var mySwiper = new Swiper(".is-slider-controls", {
+    direction: "horizontal",
+    loop: false,
+    slidesPerView: "auto",
+    spaceBetween: 16,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<div class="' + className + '"></div>';
+      }
+    },
+    navigation: {
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev",
+    },
+  });
 });
